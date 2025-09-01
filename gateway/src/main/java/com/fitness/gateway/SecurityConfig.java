4e9ee3016759d580f48b80b9ef3625d85e409b57
Hrushiingole <142684900+Hrushiingole@users.noreply.github.com>
2025-09-01 17:00:32 +0530

    Integrated oauth2.0 using keycloack. gateway is secured.

---
+++ gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.fitness.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilerChain(ServerHttpSecurity httpSecurity){
+        return httpSecurity
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+//                        .pathMatchers("/actuator/*").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2-> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
